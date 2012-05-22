18a8acd6d6e46d1852614a5a8f16142d77a058fe
Pranav Sakulkar <pranav.sakulkar@gmail.com>
2012-05-22 16:58:57 +0200

    eclipse

--- one.py
+++ one.py
@@ -1,2 +1,25 @@
+#!/usr/bin/python
+
 #this is just test file
-echo 'time pass';
+
+class Employee:
+    'Common base class for all employees'
+    count = 0;
+
+    def __init__(self,name,salary):
+        self.name = name;
+	self.salary = salary;
+	Employee.count = Employee.count + 1;
+
+    def displayCount(self):
+	print "total number of employees", Employee.count;
+
+    def displayProfile(self):
+	print "name =", self.name, "/n salary", self.salary;
+
+"execution starts from here"
+emp1 = Employee("Praveen",12000);
+emp2 = Employee("Radha",23000);
+emp1.displayProfile();
+emp2.displayCount();
+#Employee.displayCount();
---
+++ two.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+from gnuradio import gr;
+
